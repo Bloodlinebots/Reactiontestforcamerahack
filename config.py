a4fe9eb5125a76ed4e0bc8d722a8a922f8d9a84f
Bloodlinebots <githubsbots@gmail.com>
2024-12-13 04:06:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,3 @@ API_ID = "24072444"
 API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6726372149"))
-SUPPORT_GRP = "@FRIENDSHUBCHATZONE"
-UPDATE_CHNL = "ll_BOTCHAMBER_ll"
-OWNER_USERNAME = "ll_RADHE7_ll"
-    
